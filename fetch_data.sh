cfba14d0da7979f719685b809abba33f9e808a75
csae8092 <peter.andorfer@oeaw.ac.at>
2022-06-17 14:06:48 +0200

    puhh

--- fetch_data.sh
+++ fetch_data.sh
@@ -7,6 +7,7 @@ wget https://github.com/acdh-oeaw/Hermann-Bahr_Arthur-Schnitzler/archive/refs/he
 unzip master
 ant -f copy-task.xml
 rm master.zip
+./dl_imprint.sh
 
 echo "add # to @refs"
 find ./data/editions/ -type f -name "*.xml"  -print0 | xargs -0 sed -i -e 's@ref="pmb@ref="#pmb@g'
